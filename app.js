3f35dca6fc38d50cca3d482d4d77e0ac039af2e6
nicolasgasco <nicolas.gasco92@gmail.com>
2021-03-31 23:37:05 +0200

    Add get and post for cars

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
-const cars = require("./cars");
 
+const cars = require("./routes/cars");
+
+MongoClient.connect("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.1ybr6.mongodb.net/bootcamp_project2?retryWrites=true&w=majority", function(err, client) {
+    if ( err != null ) {
+        res.send(err);
+        return;
+    } 
+
+    app.locals.db = client.db("bootcamp_project2");
+    console.log("Connected to database...");
+});
 
 app.use(express.static("public"));
 app.use(express.json());
 
-app.use("/api/cars", cars);
+app.use("/api/cars/", cars);
 
 
 app.listen(3000);
--- routes/cars.js
+++ routes/cars.js
@@ -1,5 +1,40 @@
-const express = require(express);
+const express = require("express");
 const router = express.Router();
+
+
+router.get("/", ( req, res ) => {
+
+    let db = req.app.locals.db;
+
+    db.collection("cars").find().toArray( (err, allCars ) => {
+        if ( err !== null ) {
+            res.send(err);
+        }
+        
+        if ( allCars.length === 0 ) {
+            res.send( { msg: "Database is empty" } );
+        }
+
+        res.send( { results: allCars } )
+    });
+});
+
+
+router.post("/", ( req, res ) => {
+
+    let db = req.app.locals.db;
+
+    const newCar = req.body;
+
+    db.collection("cars").insertOne( newCar, (err, result ) => {
+        if ( err !== null ) {
+            res.send(err);
+        }
+        
+        res.send( { results: result.ops } )
+    });
+});
+
 
 
 module.exports = router;
