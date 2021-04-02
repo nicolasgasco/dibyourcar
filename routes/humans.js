75fa65e55af18378a1defeab7e42dad62e489f29
nicolasgasco <nicolas.gasco92@gmail.com>
2021-04-02 15:44:49 +0200

    Add new route for searching humans depending on country and or city

--- routes/humans.js
+++ routes/humans.js
@@ -20,6 +20,38 @@ router.get("/", ( req, res ) => {
     });
 });
 
+// Get all humans
+router.get("/:country/:city?", ( req, res ) => {
+
+    let db = req.app.locals.db;
+
+    let country = req.params.country;
+    country = country.charAt(0).toUpperCase() + country.substring(1).toLowerCase();
+    
+
+    let city = req.params.city;
+    ( city ) ? city = city.charAt(0).toUpperCase() + city.substring(1).toLowerCase() : city = null; 
+    
+    let searchFilterCountry = { "currently_in.country" : country};
+    let searchFilterCity;
+    if ( city ) {
+        searchFilterCity = { "currently_in.city" : city};
+    }
+
+    db.collection("humans").find( searchFilterCountry, searchFilterCity ).toArray( (err, allHumans ) => {
+        if ( err !== null ) {
+            res.send(err);
+        }
+        
+        if ( allHumans.length === 0 ) {
+            res.send( { msg: "Database is empty" } );
+        }
+
+        res.send( { results: allHumans } )
+    });
+});
+
+
 // Insert new human
 router.post("/", ( req, res ) => {
 
