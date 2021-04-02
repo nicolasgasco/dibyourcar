6627eb3387b88865cc107639940035342c5b09c6
nicolasgasco <nicolas.gasco92@gmail.com>
2021-04-02 14:54:06 +0200

    Created country + city selector

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ MongoClient.connect(`mongodb+srv://m001-student:${process.env.MONG_PASS}@sandbox
     } 
 
     app.locals.db = client.db("bootcamp_project2");
-    console.log(`Connected to database ${app.locals.db}...`);
+    console.log(`Connected to database...`);
 });
 
 app.use(express.static("public"));
@@ -24,7 +24,6 @@ app.use(express.json());
 // External routes
 app.use("/api/humans/", humans);
 app.use("/api/locations/", locations);
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
