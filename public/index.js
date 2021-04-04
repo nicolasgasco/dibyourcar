cf8e5e45b05d3f52d70997afdd6a69f2890d3ee4
nicolasgasco <nicolas.gasco92@gmail.com>
2021-04-04 20:07:25 +0200

    Added hidden menus

--- public/index.js
+++ public/index.js
@@ -1,23 +1,47 @@
 
 function showLoginContainer() {
+
+
     loginLink.removeEventListener("click", showLoginContainer);
 
     loginContainer.style.display = "block";
+    signupContainer.style.display = "none";
+
 
     loginLink.addEventListener("click", hideLoginContainer);
-
 }
 
+
 function hideLoginContainer() {
-    console.log('ciao')
     loginLink.removeEventListener("click", hideLoginContainer);
 
     loginContainer.style.display = "none";
+    signupContainer.style.display = "none";
+
 
     loginLink.addEventListener("click", showLoginContainer);
 }
+
+
+function showSignupContainer() {
+    loginContainer.style.display = "none";
+    signupContainer.style.display = "block";
+}
+
+
 
 const loginLink = document.querySelector("#login-link");
 loginLink.addEventListener("click", showLoginContainer);
 
 const loginContainer = document.querySelector("#log-in-container");
+
+const signupLink = document.querySelector("#signup-link");
+signupLink.addEventListener("click", showSignupContainer)
+
+const signupContainer = document.querySelector("#sign-up-container");
+
+const firstStartHelpLink = document.querySelector("#start-help-link");
+firstStartHelpLink.addEventListener("click", showSignupContainer);
+
+const secondStartHelpLink = document.querySelector("#start-helping-link");
+secondStartHelpLink.addEventListener("click", showSignupContainer);
