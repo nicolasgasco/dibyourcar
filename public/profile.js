1768cc29c8bf5c6341663be5c3d567e5ee13329e
nicolasgasco <nicolas.gasco92@gmail.com>
2021-04-11 10:28:37 +0200

    Deleted all unutilized code

--- public/profile.js
+++ public/profile.js
@@ -37,6 +37,7 @@ let yourSubmissions = document.querySelector("#your-submissions");
 // When loading page, check if user is already logged in, If not, redirect to home
 checkUserAlreadyLoggedIn();
 
+
 // Initialize all the button events at first
 initializeButtonEvents();
 // Show all the submitted post or default message
@@ -1007,137 +1008,27 @@ async function saveEdits(event) {
 }
 
 // From here on, it's just login logics
-// Toggle on or off the hidden container for logging in
-
-// Login link in right top corner
-const loginLink = document.querySelector("#login-link");
-loginLink.addEventListener("click", showHideLoginContainer);
-
-// Login container and form
-const loginContainer = document.querySelector("#log-in-container");
-const loginForm = document.querySelector("#log-in-form");
-
-// Login button
-const loginButton = document.querySelector("#login-button");
-loginButton.addEventListener("click", tryToLogin);
+// In this page, you can't neither login nor signin, if not logged, you get redirected to home
 
 // User icon and data container
 const userIconLink = document.querySelector("#user-icon-button")
 const userContainer = document.querySelector("#user-container");
 
-
 // Logout button
 const logoutButton = document.querySelector("#logout-button");
 logoutButton.addEventListener("click", logoutUser)
 
-function showHideLoginContainer() {
-    // if ( !signupContainer.classList.contains("hidden") ) {
-
-    //     signupContainer.classList.toggle("hidden");
-        
-    // } else {
-    loginContainer.classList.toggle("hidden");
-    // }
-}
-
-
-// Toggle on or off the hidden container for signing up
-function tryToLogin(event) {
-    event.preventDefault();
-
-    
-    // USe form validation without submitting it
-    loginForm.checkValidity();
-
-    // If all data is fine
-    if ( loginForm.reportValidity() ) {
-        
-        let userObject = {};
-        // Extract info from form
-        for ( let inputField of loginForm.children ) {
-
-            // Relevant inputs are nested inside labels for formatting reasons
-            if ( inputField.tagName === "LABEL" ) {
-
-                let value = inputField.firstElementChild.value;
-                const name = inputField.firstElementChild.name;
-                
-                switch ( name ) {
-                    case "email":
-                        userObject["email"] = value;
-                        break;
-                    case "password":
-                        userObject["password"] = value;
-                        break;
-                }
-            }
-        }
-
-        fetch("/api/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify( userObject ),
-        })
-        .then(response => response.json())
-        .then(data => {
-
-            if ( data.loginDataCorrect ) {
-                window.alert("Welcome back!")
-
-                // Use local storage to know which user is connected
-                localStorage.setItem('user', userObject.email);
-
-                // Change page to show a logged user
-                loginSuccessful();
-            } else {
-
-                // Show text for wrong e-mail/password
-                const wrongEmailPasswordText = document.querySelector("#wrong-email-password-text");
-                wrongEmailPasswordText.classList.toggle("hidden");
-
-                // Add a red outline for wrong elements
-                const passwordFieldForm = document.querySelector("#password-login");
-                passwordFieldForm.classList.toggle("red-outline");
-
-                const emailFieldForm = document.querySelector("#email-login");
-                emailFieldForm.classList.toggle("red-outline");
-
-                // After a while, make the text and the outline disappear
-                setTimeout( function() {
-                    wrongEmailPasswordText.classList.toggle("hidden");
-                    passwordFieldForm.classList.toggle("red-outline");
-                    emailFieldForm.classList.toggle("red-outline");
-                }, 2500);
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-    };
-}
-
-
 // Adapt page when login is successful
 function loginSuccessful(event) {
-    loginLink.classList.toggle("hidden");
-
-    // Need these like this because you get here from different places, toggle isn't always what you need
-    loginContainer.style.display = "none";
-    // signupContainer.style.display = "none";
-
-
-    // Show button with profile icon
-    userIconLink.style.display = "block";
     
     // Show dropdown menu with user info when clicking on icon
+    const userIconLink = document.querySelector("#user-icon-button")
     userIconLink.addEventListener("click", showDropdownUserProfile);
 
     // Adapt personal data shown in container and posts shown in container
     populatePersonalData();
-    showDefaultMessageYourSubmissions();
-
+    // This was Showdefaultmessage before, but I think it was wrong
+    showSubmittedPosts();
 }
 
 
@@ -1163,6 +1054,8 @@ function showDropdownUserProfile(event) {
         if ( user.userFound ) {
             // Show personalized message
             userName = `${user.results.name} ${user.results.surname}`;
+
+            const userContainer = document.querySelector("#user-container");
             userContainer.firstElementChild.innerText = `Hi, ${userName}`;
 
             userContainer.classList.toggle("hidden");
@@ -1176,6 +1069,7 @@ function showDropdownUserProfile(event) {
     });
 }
 
+
 function logoutUser() {
 
     fetch("/api/logout/", {
@@ -1186,7 +1080,6 @@ function logoutUser() {
     })
     .then( res => res.json() )
     .then( result => {
-        console.log(result)
 
         if ( result.loggedOut ) {
 
@@ -1209,7 +1102,6 @@ function checkUserAlreadyLoggedIn() {
     fetch("/api/check/")
     .then( res => res.json() )
     .then( userData => {
-        console.log("isLogged", userData.isLogged)
 
         if ( userData.isLogged ) {
             loginSuccessful();
@@ -1222,144 +1114,3 @@ function checkUserAlreadyLoggedIn() {
         console.error('Error:', error);
     });
 }
-
-
-
-// Toggle on and off container with signup form
-// function showHideSignupContainer() {
-
-//     // If container is not already hidden
-//     if ( !loginContainer.classList.contains("hidden") ) {
-
-//         loginContainer.classList.toggle("hidden");
-//     }
-    
-//     // signupContainer.classList.toggle("hidden");
-// }
-
-
-
-// Start signup process
-// function tryToSignup(event) {
-//     event.preventDefault();
-
-//     // Use form validation without submitting it
-//     signupForm.checkValidity();
-//     if ( signupForm.reportValidity() ) {
-
-//         let userObject = {};
-
-//         // Need this to check if passwords are identical
-//         let password;
-//         let confirmPassword;
-
-//         // Relevant inputs are nested inside labels
-//         for ( let inputField of signupForm.children ) {
-//             if ( inputField.tagName === "LABEL" ) {
-
-//                 let value = inputField.firstElementChild.value;
-//                 const name = inputField.firstElementChild.name;
-                
-//                 switch ( name ) {
-//                     case "name":
-//                         userObject["name"] = value;
-//                         break;
-                    
-//                     case "surname":
-//                         userObject["surname"] = value;
-//                         break;
-//                     case "email":
-//                         userObject["email"] = value;
-//                         break;
-
-//                     case "password":
-//                         password = value;
-//                         break;
-
-//                     case "confirm-password":
-//                         confirmPassword = value;
-//                         break;
-//                 }
-        
-//             }
-//         }
-
-        
-        
-//         // Check if password is valid, but only if field is not empty   
-//         if ( !isPasswordValid(password) ) {
-//             showPasswordNotValid();
-//             return;
-//         }
-
-//         // Check if password are equals
-//         if ( password === confirmPassword ) {
-//             userObject["password"] = password;
-//         } else {
-//             showPasswordDontMatchMessage();
-//             return;
-//         }
-
-//         // Write user to database
-//         fetch("/api/signin/", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify( userObject ),
-//         })
-//         .then(response => response.json())
-//         .then(data => {
-
-//         // Successful signup
-//         if ( data.success ) {
-
-//             window.alert(`Sign up successful!`)
-//             localStorage.setItem('user', userObject.email);
-//             populatePersonalData();
-
-//             // Do the same as for logged user
-//             loginSuccessful();
-
-//         }
-
-//         // Wrong data
-//         if ( !data.new_user ) {
-
-//             // Add red outline to e-mail field
-//             const emailField = document.querySelector("#email");
-//             emailField.classList.toggle("red-outline");
-            
-//             // Show message
-//             const userAlreadyExistsText = document.querySelector("#user-already-exists-text");
-//             userAlreadyExistsText.classList.toggle("hidden");
-
-//             // Make everything disappear after a while
-//             setTimeout( function() {
-
-//                 emailField.classList.toggle("red-outline");
-//                 userAlreadyExistsText.classList.toggle("hidden");
-//             }, 2500);
-//         }
-//         })
-//         .catch((error) => {
-//             console.error('Error:', error);
-//         });        
-//     }
-// }
-
-
-// Signup container and form
-// const signupContainer = document.querySelector("#sign-up-container");
-// const signupForm = document.querySelector("#sign-up-form");
-
-// Signup link
-// const signupLink = document.querySelector("#signup-link");
-// signupLink.addEventListener("click", showHideSignupContainer)
-
-// Signup button
-// const signupButton = document.querySelector("#signup-button");
-// signupButton.addEventListener("click", tryToSignup)
-
-
-
